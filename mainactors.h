105b4ad69a5e9fa32a3fe829b9d70f1ab3b2b57f
anasss-sia <146112230+anasss-sia@users.noreply.github.com>
2024-04-05 22:45:03 +0300

    Update mainactors.h

--- mainactors.h
+++ mainactors.h
@@ -1,16 +1,41 @@
-#ifndef MAINACTORS_H
-#define MAINACTORS_H
+#ifndef MAINACTORS_H 
 
-#include <string>
+#define MAINACTORS_H  
+#include <iostream>
+using namespace std;
 
-class MainActor {
+class Mainactors {
+private:
+
+    string a_name;
+    int a_year;
+
+
 public:
-    std::string firstName;
-    std::string lastName;
-    int birthYear;
-    std::string awards;
 
-    MainActor(std::string first, std::string last, int year, std::string a);
+    string get_aname() {
+        return a_name;
+    }
+    int get_ayear() {
+        return a_year;
+    }
+
+
+    void set_aname(string& a_name) {
+        this->a_name = a_name;
+    }
+    void set_ayear(int a_year) {
+        this->a_year = a_year;
+    }
+
+    Mainactors() {
+        a_name = string("Joe");
+        a_year = 0;
+    }
+
+    Mainactors(string& name, int year) {
+        this->a_name = name;
+        this->a_year = year;
+    }
 };
-
-#endif
+#endif //MAINACTOR_H
