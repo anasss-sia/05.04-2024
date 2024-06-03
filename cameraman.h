2e7917ab3b538c142ce7fe7f04a88b49230e8b46
Lnoviikova <146034695+Lnoviikova@users.noreply.github.com>
2024-06-03 22:35:35 +0300

    Update cameraman.h

--- cameraman.h
+++ cameraman.h
@@ -1,40 +1,18 @@
-#ifndef CAMERAMAN_H 
+#ifndef CAMERAMAN_H
+#define CAMERAMAN_H
 
-#define CAMERAMAN_H  
 #include <iostream>
-using namespace std;
+#include "person.h"
 
-class  Cameraman {
-private:
-
-    string c_name;
+class Cameraman : public Person {
+protected:
     string bwork;
-
 public:
-
-    string get_cname() {
-        return c_name;
-    }
-    string get_bwork() {
-        return bwork;
-    }
-
-    void set_dname(string& c_name) {
-        this->c_name = c_name;
-    }
-    void set_bwork(string& bwork) {
-        this->bwork = bwork;
-    }
-
-    Cameraman() {
-        c_name = string("Joe");
-        bwork = string("Film");
-
-    }
-    Cameraman(string& name, string& bwork) {
-        this->c_name = c_name;
-        this->bwork = bwork;
-
-    }
+    Cameraman();
+    Cameraman(string& name, int& year, string& bwork);
+    ~Cameraman();
+    virtual string getBwork();
+    void setBwork(string& bwork);
 };
-#endif //CAMERAMAN_H
+
+#endif 
