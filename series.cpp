ea7e4fb35456593a80e3e175a96a0f455b89033f
Lnoviikova <146034695+Lnoviikova@users.noreply.github.com>
2024-06-17 21:40:17 +0300

    series.cpp

--- series.cpp
+++ series.cpp
@@ -8,3 +8,8 @@ void Series::getSeriesInfo() {
     getFilmInfo();
     cout << "Seasons: " << numOfSeasons << " - Episodes: " << numOfEpisodes << endl;
 }
+ostream& operator<<(ostream& os, const Series& f) {
+    os << "Title: " << f.title << endl;
+    os << "Release Year: " << f.releaseYear << endl;
+    os << "Rating: " << f.rating << endl;
+    return os;
