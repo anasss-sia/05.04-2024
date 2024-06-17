49519e1d9fbcdddb7bd2701d562651433a4e6767
anasss-sia <146112230+anasss-sia@users.noreply.github.com>
2024-06-17 20:51:38 +0300

    Update film.cpp

--- film.cpp
+++ film.cpp
@@ -1,10 +1,17 @@
-#include "film.h"
+#include "Film.h"
 
-using namespace std;
-
-
-Film::Film(string title, int releaseYear, double rating) : title(title), releaseYear(releaseYear), rating(rating) {}
+Film::Film(string title, int releaseYear, double rating)
+    : title(title), releaseYear(releaseYear), rating(rating) {}
 
 void Film::getFilmInfo() {
-    cout << "Film: " << title << " (" << releaseYear << ") - Rating: " << rating << endl;
+    cout << "Title: " << title << endl;
+    cout << "Release Year: " << releaseYear << endl;
+    cout << "Rating: " << rating << endl;
+}
+
+ostream& operator<<(ostream& os, const Film& f) {
+    os << "Title: " << f.title << endl;
+    os << "Release Year: " << f.releaseYear << endl;
+    os << "Rating: " << f.rating << endl;
+    return os;
 }
