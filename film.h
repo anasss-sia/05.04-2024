0e98f26c6bdf06f30596dd9b156b4a02777059d2
anasss-sia <146112230+anasss-sia@users.noreply.github.com>
2024-06-18 23:35:17 +0300

    Update film.h

--- film.h
+++ film.h
@@ -1,31 +1,21 @@
 #ifndef FILM_H
+#ifndef FILM_H
 #define FILM_H
 
-#include <iostream>
 #include <string>
-using namespace std;
 
 class Film {
 protected:
-    string title;
-    int ryear;
-    double rating;
+    std::string title;
+    int releaseYear;
 
 public:
-    Film();
-    Film(const string& title, int ryear, double rating);
-    ~Film();
-    string getTitle() const;
-    void setTitle(const string& title);
-    int getRyear() const;
-    void setRyear(int ryear);
-    double getRating() const;
-    void setRating(int rating);
-
-    friend ostream& operator<<(ostream& os, const Film& f);
+    Film(std::string t, int ry);
+    void displayInfo() const;
+    friend std::ostream& operator<<(std::ostream& os, const Film& f);
 };
 
-#endif // FILM_H
+#endif
 
 
 
