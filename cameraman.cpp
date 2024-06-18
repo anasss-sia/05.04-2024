56b8aa98fff89c3db99bb32ee1404787b2a9743d
anasss-sia <146112230+anasss-sia@users.noreply.github.com>
2024-06-18 23:29:31 +0300

    Update cameraman.cpp

--- cameraman.cpp
+++ cameraman.cpp
@@ -1,15 +1,16 @@
-#include "cameraman.h"
+#include "cameramen.h"
+#include <iostream>
 
-Cameraman::Cameraman() {
-    bwork = "NO";
+Cameraman::Cameraman(std::string n, int yob, std::string bw) : Person(n, yob), bestWork(bw) {}
+
+void Cameraman::displayInfo() const {
+    Person::displayInfo();
+    std::cout << "Best Work: " << bestWork << std::endl;
 }
-Cameraman::Cameraman(string& name, int& year, string& bwork)
-    :Person(name, year) {
-    this->bwork = bwork;
+
+std::ostream& operator<<(std::ostream& os, const Cameraman& c) {
+    os << "Name: " << c.getName() << std::endl;
+    os << "Year of Birth: " << c.getYearOfBirth() << std::endl;
+    os << "Best Work: " << c.bestWork << std::endl;
+    return os;
 }
-Cameraman::~Cameraman() {};
-string Cameraman::getBwork() {
-    return bwork;
-}
-void Cameraman::setBwork(string& bwork) {
-    this->bwork = bwork;
