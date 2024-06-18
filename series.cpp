c15e8c00563eb744b9f943365c00685d2f9f144b
anasss-sia <146112230+anasss-sia@users.noreply.github.com>
2024-06-18 19:14:25 +0300

    Update series.cpp

--- series.cpp
+++ series.cpp
@@ -8,8 +8,8 @@ void Series::getSeriesInfo() {
     getFilmInfo();
     cout << "Seasons: " << numOfSeasons << " - Episodes: " << numOfEpisodes << endl;
 }
-ostream& operator<<(ostream& os, const Series& f) {
-    os << "Title: " << f.title << endl;
-    os << "Release Year: " << f.releaseYear << endl;
-    os << "Rating: " << f.rating << endl;
+ostream& operator<<(ostream& os, const Series& s) {
+    os << "Title: " << s.title << endl;
+    os << "Release Year: " << s.releaseYear << endl;
+    os << "Rating: " << s.rating << endl;
     return os;
